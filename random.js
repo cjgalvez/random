84cb746bd35db489b9a7ccbe82c16041b5405b44
cjgalvez <carlosgalvez@MBP-de-Carlos.Home>
2015-06-14 19:30:40 +0200

    Revert "prueba"
    This reverts commit 4b629c86da604a9dc48094474410d9f2974711bb.

--- random.js
+++ random.js
@@ -1,9 +1,8 @@
 var numero = Math.random();
 
-var str = ' mayor que 0.5';
-
 if (numero <= 0.5) {
-  str = ' menor que 0.5';
+  console.log('\n' + numero + ' menor que 0.5 \n');
 }
-
-console.log('\n' + numero + str + '\n');
+else {
+  console.log('\n' + numero + ' mayor que 0.5 \n');
+}
